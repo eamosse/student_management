2aa99c6ec6120b716ed70cb25d1011a694638187
Tafita007 <80691244+Tafita007@users.noreply.github.com>
2025-04-10 12:18:44 +0300

    add update&delete

--- routes/courses.js
+++ routes/courses.js
@@ -23,4 +23,36 @@ function create(req, res) {
     });
 }
 
-module.exports = {getAll, create};
+function update(req, res) {
+    const { courseId } = req.params;
+    const { name, code } = req.body;
+
+    Course.findByIdAndUpdate(courseId, { name, code }, { new: true })
+        .then((updatedCourse) => {
+            if (!updatedCourse) {
+                return res.status(404).send('Course not found!');
+            }
+            res.json({ message: `Course updated with id ${updatedCourse.id}`, course: updatedCourse });
+        })
+        .catch((err) => {
+            res.send('Error updating course ', err);
+        });
+}
+
+function delete(req, res) {
+    const { courseId } = req.params;
+
+    Course.findByIdAndDelete(courseId)
+        .then((deletedCourse) => {
+            if (!deletedCourse) {
+                return res.status(404).send('Course not found!');
+            }
+            res.json({ message: `Course deleted with id ${deletedCourse.id}` });
+        })
+        .catch((err) => {
+            res.send('Error deleting course ', err);
+        });
+}
+
+// Exporte les fonctions
+module.exports = { getAll, create, update, delete };
