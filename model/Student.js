f72c8c3b42c0228223107c65a07567711fd89407
tinaandriantsilavina <tinaandriantsilavina@gmail.com>
2025-04-17 22:58:24 +0300

    feat/stat: add back stat

--- model/Student.js
+++ model/Student.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const StudentSchema = new Schema({
     firstName: String,
     lastName: String,
+    year: String, 
+    program: String 
 });
-
 module.exports = mongoose.model('Student', StudentSchema);
